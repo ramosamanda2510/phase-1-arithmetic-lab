2327f824c5c308704663c8da54cffca20e2af586
amanda <ramosamanda2510@gmail.com>
2022-02-24 21:34:32 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 =31
+let num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor (Math.random()*10)+1;
+
+let num3 = 16
+let num4 = 6
+const mod = num3 %= num4
+    
+let num5 = 0
+let num6 = 5
+let num7 = 10
+let num8 = 15
+let num9 = 20
+const max = Math.max(num5,num6,num7,num8,num9)
